8da4751937cce4df0ab16346957456a0d22917ba
hstct <code@elmc.at>
2023-05-24 14:09:52 +0200

    feat: add nvim autopairs plugin

--- lua/hstct/plugins/edit.lua
+++ lua/hstct/plugins/edit.lua
@@ -13,4 +13,16 @@ return {
             require("mini.splitjoin").setup()
         end,
     },
+    {
+        "windwp/nvim-autopairs",
+        config = function()
+            require("nvim-autopairs").setup({
+                disable_filetype = { "TelescopePrompt" },
+                disable_in_macro = true,
+            })
+            local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+            local cmp = require("cmp")
+            cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+        end,
+    },
 }
